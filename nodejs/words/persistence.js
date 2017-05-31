102a502be39c9d293702a8387394d1bb351b3626
nilosaj <nilosergio.assisjr@ymail.com>
2017-05-31 14:14:29 -0300

    alteracao do codigo para  incluir Redis (cache) , argumentos de chamada e promessa

--- nodejs/words/persistence.js
+++ nodejs/words/persistence.js
@@ -1,4 +1,111 @@
+
 var oracledb = require('oracledb');
 var dbConfig = require('./dbconfig.js');
-var pool;
+var redis = require("redis");
+total = 0;
+
+
+
+
+
+exports.retornaPool = function (cb) {
+    oracledb.createPool(
+        {
+            user: dbConfig.user,
+            password: dbConfig.password,
+            connectString: dbConfig.connectString,
+            poolMax: 30, // maximum size of the pool. Increase UV_THREADPOOL_SIZE if you increase poolMax
+            poolMin: 1, // start with no connections; let the pool shrink completely
+            // poolIncrement: 1, // only grow the pool by one connection at a time
+            poolTimeout: 60, // terminate connections that are idle in the pool for 60 seconds
+            // poolPingInterval: 60, // check aliveness of connection if in the pool for 60 seconds
+            queueRequests: true, // let Node.js queue new getConnection() requests if all pool connections are in use
+            queueTimeout: 120000, // terminate getConnection() calls in the queue longer than 60000 milliseconds
+            // poolAlias: 'myalias' // could set an alias to allow access to the pool via a name
+            // stmtCacheSize: 30 // number of statements that are cached in the statement cache of each connection
+            //_enableStats: true
+        },
+        function (err, pool) {
+            if (err) {
+                console.error("createPool() error: " + err.message);
+                return;
+            }
+            cb(pool);
+        }
+
+    );
+}
+
+
+exports.consultaRedis = function (id, client, indice, pool, callback) {
+    client.hgetall(id, function (err, object) {
+        if (err){
+            console.log("Erro ",err)
+        }
+
+        if (object != null) {
+           console.log((indice+1)+") Encontrada na cache a palavra '"+object.wordsample+"' para ID "+id)
+            callback(object)
+        } else {
+            console.log((indice+1)+") Não Encontrei o "+id+". Buscando no Oracle....")
+            ConsultaOracle(id, pool, indice, (retornoOracle) => {
+                client.hmset(retornoOracle.rows[0][0], {
+                    "wordsample": retornoOracle.rows[0][1],
+                    "word_length": retornoOracle.rows[0][2]
+                }, (err, res) => {
+                    if (err){
+                        console.log("Erro na atualizacao de cache ",err)
+                    }
+                    callback(res)
+                })
+               console.log("Encontrada palavra '"+retornoOracle.rows[0][1]+"' . adicionando  ao Redis com indice " + retornoOracle.rows[0][0])
+
+            })
+
+        }
+
+    });
+
+}
+
+
+
+function ConsultaOracle(id, pool, indice, callback) {
+
+    // Checkout a connection from the pool
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log('Erro na tentativa de pegar conexao = ', indice, '  ', err);
+            return;
+        }
+
+        connection.execute(
+            " SELECT ID_WORD,WORDSAMPLE , WORD_LENGTH FROM TB_WORDS WHERE  ID_WORD = :id",
+            [id], // bind variable value
+            function (err, result) {
+                if (err) {
+                    connection.close(function (err) {
+                        if (err) {
+                            console.error("Erro na tentativa de liberar a conexão", err);
+                        }
+                    });
+                    console.log('Erro na tentativa de executar a query');
+                    return;
+                }
+                //console.log("Palavra: " + result.rows[0][1] + "  Tamanho: " + result.rows[0][2] + "    Conn open: " + pool.connectionsOpen + " Conn used: " + pool.connectionsInUse);
+                connection.close(function (err) {
+                    if (err) {
+                        console.log('Deu Erro 2');
+                    } else {
+                       // console.log('Acabou ', result.rows[0][0]);
+                        callback(result);
+                    }
+                });
+            }
+        );
+    });
+
+}
+
+
 
--- nodejs/words/wordapp.js
+++ nodejs/words/wordapp.js
@@ -1,89 +1,97 @@
-var oracledb = require('oracledb');
-var dbConfig = require('./dbconfig.js');
 var persistence = require('./persistence.js');
+var redis = require("redis");
+var yargs = require("yargs");
 
 
-function init() {
-  
-  oracledb.createPool(
-    {
-      user: dbConfig.user,
-      password: dbConfig.password,
-      connectString: dbConfig.connectString,
-       poolMax: 20, // maximum size of the pool. Increase UV_THREADPOOL_SIZE if you increase poolMax
-       poolMin: 1 // start with no connections; let the pool shrink completely
-      // poolIncrement: 1, // only grow the pool by one connection at a time
-      // poolTimeout: 60, // terminate connections that are idle in the pool for 60 seconds
-      // poolPingInterval: 60, // check aliveness of connection if in the pool for 60 seconds
-      // queueRequests: true, // let Node.js queue new getConnection() requests if all pool connections are in use
-      // queueTimeout: 60000, // terminate getConnection() calls in the queue longer than 60000 milliseconds
-      // poolAlias: 'myalias' // could set an alias to allow access to the pool via a name
-      // stmtCacheSize: 30 // number of statements that are cached in the statement cache of each connection
-    },
-    function(err, pool) {
-      if (err) {
-        console.error("createPool() error: " + err.message);
-        return;
-      }
-     
-     for (var i=0;i< 10000;i++){
-            //console.log("pool criado ",pool);
-            testeConsulta(geraNumeroAleatorio(1,800),pool,(retorno)=>{
-                console.log(retorno)
-            })
-     } 
-     
+var inputargs = yargs.argv;
 
-    }
-  );
-}
+var datainicio = getDateTime()
 
-function testeConsulta(id,pool,callback){
-// Checkout a connection from the pool
-  pool.getConnection(function(err, connection) {
-    if (err) {
-      console.log('Deu Erro neste ponto 1');
-      return;
-    }
-
-     console.log("Connections open: " + pool.connectionsOpen);
-     console.log("Connections in use: " + pool.connectionsInUse);
-
-    connection.execute(
-      " SELECT WORDSAMPLE FROM TB_WORDS WHERE  ID_WORD = :id",
-      [id], // bind variable value
-      function(err, result) {
-        if (err) {
-          connection.close(function(err) {
-            if (err) {
-              // Just logging because handleError call below will have already
-              // ended the response.
-              console.error("execute() error release() error", err);
-            }
+if (inputargs.idword != null){
+      persistence.retornaPool((pool) => {
+          client = redis.createClient(32768, "localhost");
+          client.on("error", function (err) {
+              console.log("Error " + err);
           });
-          console.log('Deu Erro 2');
-          return;
+          console.log("######## Buscando por "+inputargs.idword+"########") 
+          persistence.consultaRedis(inputargs.idword, client, 0, pool, (retornoRedis) => {
+                
+                    client.quit();
+                    console.log("Inicio :"+datainicio+"   Fim :"+getDateTime())
+                
+
+          })
+        
+      })
+
+
+}else{
+  persistence.retornaPool((pool) => {
+    client = redis.createClient(32768, "localhost");
+
+    client.on("error", function (err) {
+      console.log("Error " + err);
+    });
+    console.log("######## Busca em Massa ########")
+    
+    var execucoes = 0
+    if (inputargs.execucoes != null){
+      execucoes = inputargs.execucoes
+    }else{
+      execucoes= 10
+    }
+    
+    let proms = []
+    for (let i = 0; i < execucoes; i++) {
+
+      setTimeout(()=>{persistence.consultaRedis(geraNumeroAleatorio(1, 1699), client, i, pool, (retornoRedis) => {
+
+        proms.push(retornoRedis)
+        if (proms.length == execucoes) {
+          client.quit();
+          console.log("Inicio :"+datainicio+"   Fim :"+getDateTime())
         }
 
-        
-        /* Release the connection back to the connection pool */
-        connection.close(function(err) {
-          if (err) {
-            console.log('Deu Erro 2');
-          } else {
-            //console.log('Acabou ',result['rows']);
-            callback(result['rows']);
-          }
-        });
-      }
-    );
-  });
+      })},geraNumeroAleatorio(10,50))
+
+    }
+
+
+  })
 
 }
 
 
-function geraNumeroAleatorio(minimo , maximo){
-    return Math.floor(Math.random()*(maximo-minimo+1)+minimo);
+
+
+
+function geraNumeroAleatorio(minimo, maximo) {
+  return Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 }
 
-init()
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
